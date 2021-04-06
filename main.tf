0bcddc6cca638943864348d14aee293da04f0593
naveenkumarcgr <naveenkumar.cgr@gmail.com>
2021-04-05 21:28:02 -0700

    first commit

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
-provider "aws" {
-    region = "us-east-1"
-}
+
 
 resource "aws_instance" "mynavec2" {
-  ami = "ami-0742b4e673072066f"
+  ami = var.default
+  count = 2
   instance_type = "t2.micro"
   tags = {
     "Name" = "myNavec2Name"
   }
+
 }
 
 output "mynavec2ipaddress" {
-  value = aws_instance.mynavec2.public_ip
+  value = aws_instance.mynavec2[*].public_ip
 }
 
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "default" {
+   default = "ami-0742b4e673072066f"
+   type = string
+}
+
